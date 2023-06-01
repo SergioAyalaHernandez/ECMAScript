d97695f0dac3a50be28a24bcd36c182a796badc2
Sergio Ayala <sergio.ayala@aldeamo.com>
2023-06-01 10:06:31 -0500

    se agregan ec13

---
+++ src/ec12/01-replaceAll.js
@@ -0,0 +1,7 @@
+const string = "JavaScript es un maravilloso lenguaje de programación";
+
+const replaceString = string.replace("JavaScript", "Perro");
+
+console.log(string);
+console.log(replaceString);
+
